d240eb571ac4cdb31c6fa3b8367d98e2554c26ea
serjsen <serjsenjob@gmail.com>
2017-04-03 12:52:35 +0300

    +Добавлена отправка письма

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -13,4 +13,34 @@ class SiteController
 
         return true;
     }
+    public function actionContact() {
+
+        $userEmail = '';
+        $userText = '';
+        $result = false;
+
+        if (isset($_POST['submit'] )) {
+            $userEmail = $_POST['email'];
+            $userText = $_POST['text'];
+
+            $errors = false;
+
+            if (!User::checkEmail($userEmail)) {
+                $errors[] = 'Неправильный email';
+            }
+
+            if ($errors == false) {
+
+                $adminMail = 'henrick84@mail.ru';
+                $subject = 'Тема письма';
+                $message = 'Текст: {$userText}. От {$userEmail}.';
+                $result = mail($adminMail, $subject, $message);
+                $result = true;
+            }
+
+        }
+        require_once(ROOT . '/views/site/contacts.php');
+
+        return true;
+    }
 }
