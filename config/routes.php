ff685192986a93b985fb1e291d232069aebc243e
serjsen <serjsenjob@gmail.com>
2017-04-04 11:45:20 +0300

    +Добавлена корзина

--- config/routes.php
+++ config/routes.php
@@ -17,5 +17,14 @@ return [
     'cabinet/edit' => 'cabinet/edit',
     'cabinet' => 'cabinet/index',
 
+    'contacts' => 'site/contact',
+
+
+    'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1',
+    'cart/add/([0-9]+)' => 'cart/add/$1',
+    'cart/delete/([0-9]+)' => 'cart/delete/$1',
+    'cart/checkout' => 'cart/checkout',
+    'cart' => 'cart/index',
+
     '' => 'site/index',
 ];
